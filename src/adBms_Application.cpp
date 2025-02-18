e6a614972dcf8600892de49c857b15efe19f3912
Nate Tan <47701264+tan-nate@users.noreply.github.com>
2025-02-18 13:27:30 -0800

    change print delay

--- src/adBms_Application.cpp
+++ src/adBms_Application.cpp
@@ -124,8 +124,8 @@ void run_command(int cmd)
     printf("ekf current input: %f\n", BatterySOCEstimation_rev_U.In1);
 
     BatterySOCEstimation_rev_step();
-    printf("ekf soc: %f\n", BatterySOCEstimation_rev_B.ImpAsg_InsertedFor_SOC_at_inpor);
-    wait_us(30000000);  // 20 second delay
+    printf("ekf soc: %f\n\n\n", BatterySOCEstimation_rev_B.ImpAsg_InsertedFor_SOC_at_inpor);
+    wait_us(1000000);  // 20 second delay
     break;
   }
 
