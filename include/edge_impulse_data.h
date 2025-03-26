1e7f306e6fbe6cfac460896e37e13520350b5d37
Nate Tan <47701264+tan-nate@users.noreply.github.com>
2025-03-26 13:08:48 -0700

    fix raw_data_buffer declare

--- include/edge_impulse_data.h
+++ include/edge_impulse_data.h
@@ -4,6 +4,6 @@
 // Notice we do NOT include "metadata.h" here!
 
 // 2) Externally declare your raw buffer (no size).
-extern float raw_data_buffer[60];
+extern float raw_data_buffer;
 
 #endif // EDGE_IMPULSE_DATA_H
