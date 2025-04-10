659fa52926c2e74c5ccb2c79365981a1dcc17643
Nate Tan <47701264+tan-nate@users.noreply.github.com>
2025-04-10 14:33:14 -0700

    comment out fail; dummy spi

--- src/main.cpp
+++ src/main.cpp
@@ -28,16 +28,40 @@ SPI spi(D11, D12, D13);                     /* SPI MOSI, MISO, SCK */
 Timer timer;
 
 
-int main()
-{
-    wait_us(1000000);
-    printf("Initialization Check.\n");
-    wait_us(1000000);
+// int main()
+// {
+//     wait_us(1000000);
+//     printf("Initialization Check.\n");
+//     wait_us(1000000);
 
-    spi_init();
-    adbms_main();
+//     spi_init();
+//     adbms_main();
 
-    return 0;
+//     return 0;
+// }
+
+int main() {
+    ThisThread::sleep_for(1s);  // Let USB serial settle
+    printf("=== SPI Basic Line Check ===\n");
+
+    // Setup SPI: 8-bit, Mode 0, 2 MHz
+    spi.format(8, 0);           
+    spi.frequency(2000000);     
+
+    chip_select = 1;
+    ThisThread::sleep_for(5ms); // Ensure idle CS before start
+
+    chip_select = 0;
+    wait_us(5);                 // CS settle before SPI
+    uint8_t received = spi.write(0xAA);  // Send dummy byte
+    wait_us(5);
+    chip_select = 1;
+
+    printf("Sent: 0x%02X, Received: 0x%02X\n", 0xAA, received);
+
+    while (true) {
+        ThisThread::sleep_for(1s);
+    }
 }
 
 void spi_init()
